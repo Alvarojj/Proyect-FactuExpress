82de87e0b591aec64e5d609ae685b5d9cc27a678
Alvarojj <alvaro.jimenez@carvajal.com>
2024-10-22 09:48:32 -0500

    feature: conectando a la api factuexpress para proceso de login y registro

--- FactuExpressComponen/src/app/login/login.component.ts
+++ FactuExpressComponen/src/app/login/login.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { ServiceService } from '../service.service';
 
 @Component({
   selector: 'app-login',
@@ -12,21 +13,29 @@ import { CommonModule } from '@angular/common';
 })
 export class LoginComponent {
   loginForm = this.fb.group({
-    email: ['', [Validators.required, Validators.email]],
+    user: ['', [Validators.required]],
     password: ['', [Validators.required, Validators.minLength(6)]]
   });
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder, private service:ServiceService, private router:Router) { }
 
   onSubmit() {
     if (this.loginForm.valid) {
       console.log(this.loginForm.value);
-
+      this.service.postlogin(this.loginForm.value).subscribe(
+        (respuesta) =>{
+          console.log("Usuario correcto")
+          this.router.navigate(['/dashboard']);
+        },
+        (error) => {
+          console.log("Error en las credenciales")
+        }
+      );
     } else {
       this.loginForm.markAllAsTouched();
     }
   }
 
-  get email() { return this.loginForm.get('email'); }
+  get user() { return this.loginForm.get('user'); }
   get password() { return this.loginForm.get('password'); }
 }
